b5bc9d1f1805a3eeabede393f21954efe4c76673
erickmerchant <erickmerchant@gmail.com>
2024-08-28 14:22:48 -0400

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,19 @@ use routes::{js::js_handler, proxy::proxy_handler, root::root_handler, watch::wa
 use state::State;
 use std::sync::Arc;
 use tokio::net::TcpListener;
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
 	let state = State::default();
 	let listen = state.args.listen;
 	let state = Arc::new(state);
+
+	tracing_subscriber::fmt()
+		.compact()
+		.with_max_level(tracing::Level::DEBUG)
+		.init();
+
 	let cool_api = Router::new()
 		.route("/cool-stylesheet.js", get(js_handler))
 		.route("/watch", get(watch_handler));
@@ -22,7 +29,8 @@ async fn main() {
 		.route("/", get(root_handler))
 		.nest(format!("/{}", state.args.cool_base).as_str(), cool_api)
 		.route("/*path", get(proxy_handler))
-		.with_state(state);
+		.with_state(state)
+		.layer(TraceLayer::new_for_http());
 	let listener = TcpListener::bind(("0.0.0.0", listen))
 		.await
 		.expect("should listen");
