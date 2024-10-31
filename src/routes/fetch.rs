97177152bddc4c72bd1538141c2f6faffbe457d7
erickmerchant <erickmerchant@gmail.com>
2024-10-31 10:13:42 -0400

    Simplify some control

--- src/routes/fetch.rs
+++ src/routes/fetch.rs
@@ -32,34 +32,30 @@ pub async fn fetch_handler(
 	let bytes = to_bytes(res.into_body(), usize::MAX).await?.to_vec();
 	let css = String::from_utf8(bytes)?;
 	let sourcemap = sourcemap::locate_sourcemap_reference(css.as_bytes())?;
-	let url = match sourcemap {
-		Some(sourcemap::SourceMapRef::Ref(url)) => Some(url),
-		Some(sourcemap::SourceMapRef::LegacyRef(url)) => Some(url),
-		_ => None,
-	};
-	let map = if let Some(url) = url {
-		if let Ok(map) = sourcemap::decode_data_url(url.as_str()) {
-			Some(map)
-		} else {
-			let base_url = Url::parse(format!("http://0.0.0.0:{}", state.args.port).as_str())?;
-			let base_url = base_url.join(params.pathname.as_str())?;
-			let url = base_url.join(url.as_str())?;
-			let mut new_req = Request::new("".into());
-
-			*new_req.uri_mut() = url.to_string().parse()?;
-
-			let res = fallback_handler(State(state.clone()), new_req).await?;
-			let bytes = to_bytes(res.into_body(), usize::MAX).await?.to_vec();
-			let map = String::from_utf8(bytes)?;
-
-			if let Ok(map) = sourcemap::decode(map.as_bytes()) {
+	let map = match sourcemap {
+		Some(sourcemap::SourceMapRef::Ref(url)) | Some(sourcemap::SourceMapRef::LegacyRef(url)) => {
+			if let Ok(map) = sourcemap::decode_data_url(url.as_str()) {
 				Some(map)
 			} else {
-				None
+				let base_url = Url::parse(format!("http://0.0.0.0:{}", state.args.port).as_str())?;
+				let base_url = base_url.join(params.pathname.as_str())?;
+				let url = base_url.join(url.as_str())?;
+				let mut new_req = Request::new("".into());
+
+				*new_req.uri_mut() = url.to_string().parse()?;
+
+				let res = fallback_handler(State(state.clone()), new_req).await?;
+				let bytes = to_bytes(res.into_body(), usize::MAX).await?.to_vec();
+				let map = String::from_utf8(bytes)?;
+
+				if let Ok(map) = sourcemap::decode(map.as_bytes()) {
+					Some(map)
+				} else {
+					None
+				}
 			}
 		}
-	} else {
-		None
+		_ => None,
 	};
 	let sources = match map {
 		Some(sourcemap::DecodedMap::Regular(map)) => map.sources().map(|s| s.to_string()).collect(),
