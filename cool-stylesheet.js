4cf538b8ce25df6d53ce29b0c760813bad478eb4
erickmerchant <erickmerchant@gmail.com>
2025-03-07 13:36:19 -0500

    Simplify js, and switch async stream crates

--- cool-stylesheet.js
+++ cool-stylesheet.js
@@ -1,94 +1,67 @@
-let registry = new Map();
-let sources = new Map();
 let base = new URL(import.meta.url);
 let coolBase = base.pathname.substring(0, base.pathname.lastIndexOf("/"));
 let esrc = new EventSource(`${coolBase}/watch`);
-
-esrc.addEventListener("message", async (event) => {
-	let data = JSON.parse(event.data);
-	let updates = new Set();
-
-	for (let pathname of data) {
-		pathname = new URL(pathname, base).pathname;
-
-		let sheets = sources.get(pathname);
-
-		if (!sheets) continue;
-
-		for (let sheet of sheets) {
-			updates.add(updateSheet(sheet, pathname));
-		}
-	}
-
-	await Promise.allSettled([...updates]);
-});
-
-async function getOrCreatSheet(root, index, pathname, media) {
-	media ??= "all";
-
-	let item = registry.get(pathname) ?? new Map();
-
-	registry.set(pathname, item);
-
-	let sheet = item.get(media) ?? new CSSStyleSheet({media: media});
-
-	item.set(media, sheet);
-
-	root.adoptedStyleSheets.splice(index, 1, sheet);
-
-	return sheet;
-}
-
-async function updateSheet(sheet, pathname) {
-	let url = new URL(`${coolBase}/fetch`, base);
-
-	url.searchParams.append("pathname", pathname);
-
-	let res = await fetch(url);
-	let json = await res.json();
-
-	if (json.css.includes("@import")) {
-		return;
-	}
-
-	for (let src of [pathname].concat(json.sources ?? [])) {
-		sources.set(src, sources.get(src) ?? new Set());
-
-		sources.get(src).add(sheet);
-	}
-
-	sheet.replaceSync(json.css);
-}
 
 class CoolStylesheet extends HTMLLinkElement {
 	static get observedAttributes() {
 		return ["media"];
 	}
 
+	sources = new Set();
 	pathname;
-	root;
+	stylesheet;
+
+	async update() {
+		let url = new URL(`${coolBase}/fetch`, base);
+
+		url.searchParams.append("pathname", this.pathname);
+
+		let res = await fetch(url);
+		let json = await res.json();
+
+		if (json.css.includes("@import")) {
+			return;
+		}
+
+		this.sources = new Set(json.sources);
+
+		this.stylesheet.replaceSync(json.css);
+	}
 
 	connectedCallback() {
-		this.root = new WeakRef(this.getRootNode());
-
 		let url = new URL(this.href);
 
 		if (url.host !== new URL(import.meta.url).host) return;
 
 		this.pathname = url.pathname;
 
-		let media = this.getAttribute("media");
+		esrc.addEventListener("message", async (event) => {
+			let data = JSON.parse(event.data);
 
-		getOrCreatSheet(
-			this.root.deref(),
-			this.root.deref().adoptedStyleSheets.length,
-			this.pathname,
-			media
-		).then(async (sheet) => {
-			await updateSheet(sheet, this.pathname);
+			for (let pathname of data) {
+				pathname = new URL(pathname, base).pathname;
 
-			this.disabled = true;
+				if (pathname !== this.pathname && !this.sources.has(pathname)) continue;
+
+				this.update();
+			}
 		});
+
+		let media = this.getAttribute("media") ?? "all";
+
+		this.stylesheet = new CSSStyleSheet({media: media});
+
+		let root = this.getRootNode();
+
+		root.adoptedStyleSheets.splice(
+			root.adoptedStyleSheets.length,
+			1,
+			this.stylesheet
+		);
+
+		this.update();
+
+		this.disabled = true;
 	}
 
 	async attributeChangedCallback(_, old_media, new_media) {
@@ -96,25 +69,14 @@ class CoolStylesheet extends HTMLLinkElement {
 			return;
 		}
 
-		let old_sheet = registry.get(this.pathname)?.get(old_media ?? "all");
-		let index = this.root.deref().adoptedStyleSheets.lastIndexOf(old_sheet);
-
-		let sheet = await getOrCreatSheet(
-			this.root.deref(),
-			index,
-			this.pathname,
-			new_media
-		);
-
-		await updateSheet(sheet, this.pathname);
+		this.stylesheet.media = new_media;
 	}
 
 	disconnectedCallback() {
-		let media = this.getAttribute("media");
-		let sheet = registry.get(this.pathname)?.get(media ?? "all");
-		let index = this.root.deref().adoptedStyleSheets.lastIndexOf(sheet);
+		let root = this.getRootNode();
+		let index = root.adoptedStyleSheets.lastIndexOf(this.stylesheet);
 
-		this.root.deref().adoptedStyleSheets.splice(index, 1);
+		root.adoptedStyleSheets.splice(index, 1);
 	}
 }
 
--- src/routes/watch.rs
+++ src/routes/watch.rs
@@ -1,5 +1,5 @@
 use crate::args::Commands;
-use async_stream::try_stream;
+use async_fn_stream::try_fn_stream;
 use axum::{
 	extract::State,
 	response::sse::{Event, KeepAlive, Sse},
@@ -14,7 +14,7 @@ use std::{convert::Infallible, fs::canonicalize, path, sync::Arc, time::Duration
 pub async fn watch_handler(
 	State(state): State<Arc<crate::State>>,
 ) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
-	Sse::new(try_stream! {
+	Sse::new(try_fn_stream(|emitter| async move {
 		let (mut sender, mut receiver) = channel(1);
 		let mut watcher = RecommendedWatcher::new(
 			move |res| {
@@ -23,36 +23,53 @@ pub async fn watch_handler(
 				})
 			},
 			Config::default(),
-		).expect("watcher should be created");
+		)
+		.expect("watcher should be created");
 
 		let directory = match &state.args.command {
-			Commands::Proxy { host: _host, directory } => directory,
-			Commands::Serve { directory } => directory
+			Commands::Proxy {
+				host: _host,
+				directory,
+			} => directory,
+			Commands::Serve { directory } => directory,
 		};
 
-		watcher.watch(path::Path::new(directory.as_str()), RecursiveMode::Recursive).expect("watcher should watch");
+		watcher
+			.watch(
+				path::Path::new(directory.as_str()),
+				RecursiveMode::Recursive,
+			)
+			.expect("watcher should watch");
 
 		while let Some(res) = receiver.next().await {
 			match res {
 				Ok(event) => {
-					let hrefs : Vec<_> = event.paths.iter().map(|p| {
-						let c = canonicalize(directory.as_str()).expect("path should be valid");
+					let hrefs: Vec<_> = event
+						.paths
+						.iter()
+						.filter_map(|p| {
+							let c = canonicalize(directory.as_str()).expect("path should be valid");
 
-						diff_paths(p, c).map(|p| {
-							let p = p.to_str().expect("path should be a string");
-							let base = Utf8Path::new(state.args.style_base.as_str());
-							let p = base.join(p);
+							diff_paths(p, c).map(|p| {
+								let p = p.to_str().expect("path should be a string");
+								let base = Utf8Path::new(state.args.style_base.as_str());
+								let p = base.join(p);
 
-							format!("/{p}")
+								format!("/{p}")
+							})
 						})
-					}).collect();
+						.collect();
 
-					yield Event::default().data(json!(hrefs).to_string());
-				},
+					emitter
+						.emit(Event::default().data(json!(hrefs).to_string()))
+						.await
+				}
 				Err(e) => eprintln!("watch error: {:?}", e),
 			}
 		}
-	})
+
+		Ok(())
+	}))
 	.keep_alive(
 		KeepAlive::new()
 			.interval(Duration::from_secs(10))
